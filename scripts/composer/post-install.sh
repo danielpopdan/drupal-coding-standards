3cababe84cb7087488529b28beef733ade5d0d32
Popdan Daniel <dpopdan@pitechplus.com>
2015-10-14 20:12:29 +0300

    Fix broken drupalcs command.

--- scripts/composer/post-install.sh
+++ scripts/composer/post-install.sh
@@ -2,6 +2,6 @@
 
 # Symlink the git pre-push hook to its destination.
 if [ ! -h ".git/hooks/pre-push" ] ; then
-  ln -s "../../vendor/drupal-pfrenssen/phpcs-pre-push/pre-push" ".git/hooks/pre-push"
+  ln -s "../../vendor/drupal-standards/drupal-coding-standards/pre-push" ".git/hooks/pre-push"
   vendor/bin/phpcs --config-set installed_paths vendor/drupal/coder/coder_sniffer
 fi
